237c68847a74d1687224d0fbf7322e8e7a4a2f8e
huguettekabemba <kab21012@byui.edu>
2023-03-30 23:17:05 +0200

    com

--- Week-7/comments.js
+++
@@ -1,9 +0,0 @@
-export class Comment {
-    constructor(hikeId, comment, type='hike') {
-        this.comment = comment,
-        this.hikeId = hikeId,
-        this.type = type,
-        this.createdDate = new Date(),
-        this.commentId = Date.now()
-    }
-}
